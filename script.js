ece41eb55f5c3a75c01dc2ac9db71eab47995e09
callumpye05 <164021930+callumpye05@users.noreply.github.com>
2026-02-27 13:27:15 +0100

    added basic background changes and slight UX revamp

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+
+
+/* --- SCROLL REVEAL LOGIC --- */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            
+            entry.target.classList.add('show');
+        } else {
+           
+           
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    threshold: 0.15 
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
