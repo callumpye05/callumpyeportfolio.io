045e713d229b2113abd1be9fe0ff1762e42e0d10
callumpye05 <164021930+callumpye05@users.noreply.github.com>
2026-02-27 12:37:19 +0100

    Changed project segment + updated about section

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let currentCardIndex = 0;
+
+function moveDeck(direction) {
+    const cards = document.querySelectorAll('.project-card');
+    cards[currentCardIndex].classList.remove('active');
+    currentCardIndex += direction;
+    if (currentCardIndex >= cards.length) {
+        currentCardIndex = 0;
+    } else if (currentCardIndex < 0) {
+        currentCardIndex = cards.length - 1;
+    }
+    cards[currentCardIndex].classList.add('active');
+}
+
+
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
